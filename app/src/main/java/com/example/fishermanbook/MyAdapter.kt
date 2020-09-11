b93f2636cf47f347bd63fb2f92e6e00b6ac1d0ac
vanol <vanolincikus@gmail.com>
2020-09-11 22:33:48 +0300

    baits addded

--- app/src/main/java/com/example/fishermanbook/MyAdapter.kt
+++ app/src/main/java/com/example/fishermanbook/MyAdapter.kt
@@ -23,13 +23,14 @@ class MyAdapter(listArray: ArrayList<ListItem>, context: Context) :
 
         fun bind(listItem: ListItem, context: Context) {
             tvTitle.text = listItem.titleText
-            tvContent.text = listItem.contentText
+            var shorDescription= listItem.contentText.substring(0,50)+ "..."
+            tvContent.text = shorDescription
             image.setImageResource(listItem.image_id)
             itemView.setOnClickListener() {
                 Toast.makeText(context, "Pressed:  ${tvTitle.text}", Toast.LENGTH_SHORT).show()
                 val i = Intent(context, ContentActivity::class.java).apply {
                     putExtra("title", tvTitle.text.toString())
-                    putExtra("content", tvContent.text.toString())
+                    putExtra("content", listItem.contentText)
                     putExtra("image", listItem.image_id)
                 }
                 context.startActivity(i)
